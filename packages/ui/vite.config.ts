13a87f87ad5ad0f9084937d0ce8265b574cc5dd8
James Dumbell <james.dumbell@grafana.com>
2023-09-30 18:58:04 +0100

    move ui to sub package

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,9 +1,11 @@
+/// <reference types="vitest/config" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
 
-// https://vitejs.dev/config https://vitest.dev/config
+// https://vitejs.dev/config
 export default defineConfig({
   plugins: [svgr(), react(), tsconfigPaths()],
   test: {
