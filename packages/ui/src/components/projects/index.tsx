97ad4d3c6735863cb2f908d4338a86e08733328f
James Dumbell <james.dumbell@grafana.com>
2026-01-14 22:48:36 +0000

    .

--- packages/ui/src/components/projects/index.tsx
+++ packages/ui/src/components/projects/index.tsx
@@ -90,7 +90,7 @@ const ProjectCard: FC<CardProject> = ({
   npm
 }) => {
   return (
-    <Card className="m-1 flex h-72 w-80 flex-col justify-between border-none bg-gray-900 p-5 transition duration-300 hover:scale-105">
+    <Card className="m-1 flex h-72 w-80 flex-col justify-between border-none bg-gray-900 p-5">
       <div>
         <div className="flex justify-end">
           {npm && (
